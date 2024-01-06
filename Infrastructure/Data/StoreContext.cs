9a3f792f76536c175052d21c00deedba875798ce
Jan Matuszak <j.matuszak.628@studms.ug.edu.pl>
2024-01-06 02:47:10 +0100

    EoS3 Adding types and brands entities. Seeding data. Creating repository and interfaces.

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,7 +1,8 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data
+namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
@@ -10,5 +11,13 @@ namespace API.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
