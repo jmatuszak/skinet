094f3a8e0d8169f8674d922e88fe05267d63741a
Jan Matuszak <j.matuszak.628@studms.ug.edu.pl>
2024-01-11 01:42:11 +0100

    s8 angular interfaces, navbar-component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient){
+  
+  }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
+      next: (response: any) => this.products = response.data, // what to do next
+      error: error => console.log(error), //what to do if there is  anerror
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+
+      }
+    })
+  }
+  
 }
